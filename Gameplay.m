eee1613ced502c81cd5453b28dce570151970ae8
Jesus Magana <maganazero5@gmail.com>
2014-08-11 22:52:08 -0700

    Added springs

--- Gameplay.m
+++ Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 
 // is called when CCB file has completed loading
@@ -22,6 +23,12 @@
     
     CCScene *level = [CCBReader load:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
+    
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
 }
 
 // called on every touch in this scene
