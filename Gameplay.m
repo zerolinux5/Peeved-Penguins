3f6713e3c583c13a9ea03ac593c917511934ed81
Jesus Magana <maganazero5@gmail.com>
2014-08-11 22:35:54 -0700

    Fixed the reset button and made objects part of a node

--- Gameplay.m
+++ Gameplay.m
@@ -12,6 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
@@ -45,7 +46,7 @@
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 -(void)retry{
